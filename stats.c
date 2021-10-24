a9ad489e8e5d6a49945ab2a77b74893ec685d7b8
Rajkumar D <91065062+Duraisamy-Rajkumar@users.noreply.github.com>
2021-10-24 12:59:46 +0530

    average, min , max calculation

--- stats.c
+++ stats.c
@@ -5,6 +5,30 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    
+    unsigned char count=0;
+    float total=0;
+    for (count=0;count<setlength;count++)
+    {
+        total=total+numberset[count];
+    }
+    s.average = total/setlength;
+    
+    count=0;
+    s.min=numberset[count];
+    s.max=numberset[count];
+    for (count=1;count<setlength;count++)
+    {
+        if(s.min>numberset[count])
+        {
+            s.min=numberset[count];
+        }
+        
+        if(s.max<numberset[count])
+        {
+            s.max=numberset[count];
+        }
+    }
 }
 
 void check_and_alert(float maxthreshold,  alerter_funcptr alerters[], struct Stats computedStats)
